eeaf5be7a34c88a7b56061098386da92b945f21a
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-04-08 00:34:33 +0300

    feat: add Preact island component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "http://localhost:4321", // Replace with your URL
+  site: "http://localhost:4321" // Replace with your URL
+  ,
+  integrations: [preact()]
 });
